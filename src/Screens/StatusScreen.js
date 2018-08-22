8851f3b5c11c816ae1cd3f3fd305f52c6e2eeee8
Farhod <fmurodov90@mail.ru>
2018-08-22 17:51:57 +0500

    status screen

--- src/Screens/StatusScreen.js
+++ src/Screens/StatusScreen.js
@@ -1,8 +1,29 @@
 import React , {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text,ScrollView, ActivityIndicator} from 'react-native';
 import {Header, Title,Button,Right,Left ,Body} from 'native-base';
 import FontAwesome  from 'react-native-vector-icons/FontAwesome'
+const api_key='01484027a8b0d51b62199785a1bb1204';
+
 export  default class StatusScreen extends Component{
+    state={
+        baseCurrency:'',
+        ratesCurrency:[],
+        loading:false
+    }
+    fetchData = async () => {
+        try {
+            const data =  await fetch('http://data.fixer.io/api/latest?access_key=01484027a8b0d51b62199785a1bb1204&symbols=USD,RUB,UZS,GBP,BTC,AUD,CAD,PLN,MXN&format=1');
+            const json= await data.json();
+            this.setState({
+                baseCurrency: json.base,
+                ratesCurrency:json.rates
+            });
+            console.log('Base', this.state.baseCurrency);
+            console.log('Rates',this.state.ratesCurrency);
+        } catch (e) {
+            console.log('Error occured while fetching data', e)
+        }
+    }
     render(){
         return(
             <View>
@@ -12,7 +33,9 @@ export  default class StatusScreen extends Component{
                         <Title>Dashboard</Title>
                     </Body>
                     <Right>
-                        <Button transparent>
+                        <Button transparent
+                            onPress={this.fetchData}
+                        >
                             <FontAwesome
                                 size={22}
                                 name="refresh"
@@ -21,9 +44,21 @@ export  default class StatusScreen extends Component{
                         </Button>
                     </Right>
                 </Header>
-                <Text>
-                    Status Screen
-                </Text>
+                <View>
+                    <Text>
+                        {this.state.baseCurrency}
+                    </Text>
+                </View>
+                <ScrollView>
+                    {
+                        // this.state.ratesCurrency &&
+                        //     this.state.ratesCurrency.map(( item , index) => (
+                        //         <View key={index}>
+                        //             <Text>{item}</Text>
+                        //         </View>
+                        //     ))
+                    }
+                </ScrollView>
             </View>
         )
     }
