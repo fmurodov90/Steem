55c72ce172b5911bf6a6b1e4b6945e4155096a19
Farhod <fmurodov90@mail.ru>
2018-08-31 00:18:28 +0500

    Witness screnn modified

--- src/Screens/StatusScreen.js
+++ src/Screens/StatusScreen.js
@@ -1,65 +1,148 @@
-import React , {Component} from 'react';
-import {View, Text,ScrollView, ActivityIndicator} from 'react-native';
-import {Header, Title,Button,Right,Left ,Body} from 'native-base';
-import FontAwesome  from 'react-native-vector-icons/FontAwesome'
-const api_key='01484027a8b0d51b62199785a1bb1204';
+import React, {Component} from 'react';
+import {View, Text, ScrollView, ActivityIndicator, FlatList, StyleSheet} from 'react-native';
+import {Header, Title, Button, Right, Left, Body, Form, Picker, Content} from 'native-base';
+import FontAwesome from 'react-native-vector-icons/Ionicons'
 
-export  default class StatusScreen extends Component{
-    state={
-        baseCurrency:'',
-        ratesCurrency:[],
-        loading:false
+
+export default class StatusScreen extends Component {
+    state = {
+        baseCurrency: "BTC",
+        ratesCurrency: [],
+        loading: false
+    };
+
+    componentDidMount() {
+        this.fetchData()
     }
+
     fetchData = async () => {
         try {
-            const data =  await fetch('http://data.fixer.io/api/latest?access_key=01484027a8b0d51b62199785a1bb1204&symbols=USD,RUB,UZS,GBP,BTC,AUD,CAD,PLN,MXN&format=1');
-            const json= await data.json();
             this.setState({
-                baseCurrency: json.base,
-                ratesCurrency:json.rates
-            });
-            console.log('Base', this.state.baseCurrency);
-            console.log('Rates',this.state.ratesCurrency);
+                loading:true
+            })
+            const data = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${this.state.baseCurrency}&tsyms=USD,JPY,EUR,GBP`);
+            const json = await data.json();
+            let result = [];
+            for (let key in json) {
+                if (json.hasOwnProperty(key)) {
+                    result.push({
+                        key: key,
+                        value: json[key]
+                    });
+                }
+            }
+            //const result = Object.keys(json).map(key => ({ key, value: json[key] }));
+            console.log(result, typeof (result));
+            this.setState({
+                ratesCurrency: result,
+                loading:false
+            })
         } catch (e) {
-            console.log('Error occured while fetching data', e)
+           // console.log('Error occured while fetching data', e),
+                alert(`Sorry couldn't connect to server, please check your internet connection `)
         }
     }
-    render(){
-        return(
+    onValueChange = val => {
+        this.setState({
+            baseCurrency: val,
+            loading: true
+        }, this.fetchData)
+
+    }
+
+    render() {
+        return (
             <View>
-                <Header style={{backgroundColor:'#72c9ff'}}>
+                <Header style={{backgroundColor: '#72c9ff'}}>
                     <Left/>
                     <Body>
-                        <Title>Dashboard</Title>
+                    <Title>Dashboard</Title>
                     </Body>
                     <Right>
+                        <ActivityIndicator
+                            size="large"
+                            color="red"
+                            animating={this.state.loading}
+                        />
                         <Button transparent
-                            onPress={this.fetchData}
+                                onPress={this.fetchData}
                         >
                             <FontAwesome
                                 size={22}
-                                name="refresh"
+                                name="md-refresh"
                                 color="white"
                             />
                         </Button>
                     </Right>
                 </Header>
-                <View>
-                    <Text>
-                        {this.state.baseCurrency}
-                    </Text>
+                <View style={styles.baseCurrencyHeader}>
+                    <Form style={{alignItems: 'center', justifyContent: 'center'}}>
+                        <Picker
+                            note
+                            mode="dropdown"
+                            style={{width: 220, alignItems: 'center', justifyContent: 'center'}}
+                            placeholder="Select currency"
+                            textStyle={{color: 'blue'}}
+                            itemTextStyle={{color: 'blue'}}
+                            itemStyle={{alignItems: 'center', justifyContent: 'center', backgroundColor: '#ddd'}}
+                            selectedValue={this.state.baseCurrency}
+                            onValueChange={(val) => this.onValueChange(val
+                            )}
+                        >
+                            <Picker.Item label="BTC" value="BTC"/>
+                            <Picker.Item label="ETH" value="ETH"/>
+                            <Picker.Item label="DASH" value="DASH"/>
+                            <Picker.Item label="NXT" value="NXT"/>
+                            <Picker.Item label="ZEC" value="ZEC"/>
+                        </Picker>
+                    </Form>
                 </View>
                 <ScrollView>
-                    {
-                        // this.state.ratesCurrency &&
-                        //     this.state.ratesCurrency.map(( item , index) => (
-                        //         <View key={index}>
-                        //             <Text>{item}</Text>
-                        //         </View>
-                        //     ))
-                    }
+                    <FlatList
+                        data={this.state.ratesCurrency}
+                        renderItem={({item}) => (
+                            <View style={{
+                                width: '100%',
+                                borderWidth: 1,
+                                borderColor: '#aaa',
+                                height: 35,
+                                paddingTop: 2,
+                                flexDirection: 'row',
+                                paddingHorizontal: 5
+                            }}>
+                                <Text style={{
+                                    color: 'red',
+                                    fontSize: 17,
+                                    fontWeight: 'bold',
+                                    width: '75%'
+                                }}>{item.key}</Text>
+                                <Text style={{
+                                    color: 'blue',
+                                    fontSize: 15,
+                                    fontWeight: 'bold',
+                                    width: '25%'
+                                }}> {item.value}</Text>
+
+                            </View>
+                        )}
+                        keyExtractor={item => item.key}
+                    />
                 </ScrollView>
             </View>
         )
     }
 }
+const styles = StyleSheet.create({
+    baseCurrencyHeader: {backgroundColor: '#ddd',},
+    baseCurrencyHeaderText: {fontSize: 24, color: 'blue', fontWeight: 'bold'},
+    //  flatlist: {
+    //      paddingVertical: 1,
+    //      borderWidth: 1,
+    //      borderColor: '#aaa',
+    //      width: '100%',
+    //      height: '20',
+    //      flexDirection: 'row'
+    //  },
+    // flatlistKey: {color: 'red', fontSize: 20, fontWeight: 'bold'},
+
+});
--- src/Screens/WitnessesScreen.js
+++ src/Screens/WitnessesScreen.js
@@ -1,8 +1,33 @@
 import React , {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text,ActivityIndicator,ScrollView,FlatList} from 'react-native';
 import {Header, Title,Button,Right,Left ,Body} from 'native-base';
-import FontAwesome  from 'react-native-vector-icons/FontAwesome'
+import FontAwesome  from 'react-native-vector-icons/FontAwesome';
+import  * as dsteem  from 'dsteem';
 export  default class WitnessesScreen extends Component{
+    state={
+        fetching:false,
+        witnessList:[]
+    };
+    componentDidMount(){
+        this.fetchWitnessList()
+    }
+
+    fetchWitnessList = async () => {
+        try {
+            this.setState({
+                fetching: true
+            })
+            const client = new dsteem.Client('https://api.steemit.com');
+            const witnesses = await client.database.call('get_witnesses_by_vote', ["", 100]);
+            console.log('Witnesses=', witnesses);
+            this.setState({
+                witnessList: witnesses,
+                fetching: false
+            })
+        } catch (e) {
+            alert(e)
+        }
+    };
     render(){
         return(
             <View>
@@ -12,7 +37,15 @@ export  default class WitnessesScreen extends Component{
                         <Title>Witnesses</Title>
                     </Body>
                     <Right>
-                        <Button transparent>
+                        <ActivityIndicator
+                            size="large"
+                            color="red"
+                            animating={this.state.fetching}
+                        />
+                        <Button
+                            transparent
+                            onPress={this.fetchWitnessList}
+                        >
                             <FontAwesome
                                 size={22}
                                 name="refresh"
@@ -21,9 +54,33 @@ export  default class WitnessesScreen extends Component{
                         </Button>
                     </Right>
                 </Header>
-                <Text>
-                    Witnesses Screen
-                </Text>
+                <View style={{backgroundColor:'#eee', flexDirection:'row', width:'100%', height:30}}>
+                    <View style={{width:'30%', alignItems:'center', justifyContent:'center'}}><Text style={{fontSize:12, color:'blue'}} > Owner</Text></View>
+                    <View style={{width:'35%', alignItems:'center', justifyContent:'center'}}><Text style={{fontSize:12, color:'blue'}} > Sbd exchange rate</Text></View>
+                    <View style={{width:'35%', alignItems:'center', justifyContent:'center'}}><Text style={{fontSize:12, color:'blue'}} >Account creation fee</Text></View>
+                </View>
+                { this.state.witnessList &&
+                    <ScrollView>
+                        <FlatList
+                            data={this.state.witnessList}
+                            renderItem={({item}) => (
+                                <View  style={{borderWidth:1,paddingVertical:2 ,flexDirection:'row', width:'100%', height:35}}>
+                                    <View style={{width:'30%', alignItems:'center', justifyContent:'center'}}>
+                                        <Text style={{fontSize:12, color:'red'}}>{item.owner}</Text>
+                                    </View>
+                                    <View style={{width:'35%', alignItems:'center', justifyContent:'center'}}>
+                                        <Text style={{fontSize:12, color:'red'}}>{item.sbd_exchange_rate.base}</Text>
+                                    </View>
+                                    <View style={{width:'35%', alignItems:'center', justifyContent:'center'}}>
+                                        <Text style={{fontSize:12, color:'red'}}>{item.props.account_creation_fee}</Text>
+                                    </View>
+                                </View>
+                            )}
+                            keyExtractor={item => item.owner}
+
+                        />
+                    </ScrollView>
+                }
             </View>
         )
     }
